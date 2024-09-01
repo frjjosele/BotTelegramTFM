bcfdb5e73164986cedfa0917cfb35e353eb385b1
frjjosele <frjjosele@gmail.com>
2024-09-01 18:04:52 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,19 +2,19 @@ import asyncio
 import nest_asyncio
 nest_asyncio.apply()
 from telegram.ext import ApplicationBuilder
-from config import TOKEN  # Importa el token de configuración.
-import handlers  # Importa el módulo de manejadores de mensajes.
+from config import TOKEN  
+import handlers  
 
 async def main():
     # Crea una nueva aplicación de bot con el token provisto.
     application = ApplicationBuilder().token(TOKEN).build()
 
-    # Llama a la función definida en handlers.py para registrar los manejadores en la aplicación.
+   
     handlers.register_handlers(application)
 
-    # Inicia el bot para que comience a sondear mensajes.
+    # Inicia el bot
     await application.run_polling()
 
-# Punto de entrada principal de Python.
+
 if __name__ == '__main__':
     asyncio.run(main())
